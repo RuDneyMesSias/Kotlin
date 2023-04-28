ed4c99416a31d401b156e16de1827748cfd889c3
Desktop <rudney_vinicius@hotmaol.com>
2023-04-28 19:19:37 -0300

    Activity_PrimeiroApp

--- app/src/main/java/com/Rudev/androidmaster/FirstApp/PrimeiroAppActivity.kt
+++ app/src/main/java/com/Rudev/androidmaster/FirstApp/PrimeiroAppActivity.kt
@@ -2,11 +2,28 @@ package com.Rudev.androidmaster.FirstApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatEditText
 import com.example.androidmaster.R
 
 class PrimeiroAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_primeiro_app)
+
+        val btnStart = findViewById<AppCompatButton>(R.id.btnStart)
+        val etNome = findViewById<AppCompatEditText>(R.id.etNome)
+
+
+        btnStart.setOnClickListener {
+            val nome = etNome.text.toString()
+
+            if (nome.isNotEmpty()){
+                Log.i("Rudney", "Botão Pulsando $nome")
+            }
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/Rudev/androidmaster/FirstApp/ResultadoActivity.kt
@@ -0,0 +1,12 @@
+package com.Rudev.androidmaster.FirstApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidmaster.R
+
+class ResultadoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+    }
+}
